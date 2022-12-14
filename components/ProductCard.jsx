be0625dd2c5ab1e527f4cc37ad4f89a7fa2888bd
Arakito14 <inefableataraxia1@gmail.com>
2022-12-14 16:24:42 -0300

    clean

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,14 +1,10 @@
 import styles from "../styles/ProductCard.module.css"
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>MARIANNA A LA PLANCHA</h1>
-      <span className={styles.price}>$15990</span>
-      <h1 className={styles.title}>SSD A LA a</h1>
-      <span className={styles.price}>$15232990</span>
-      <h1 className={styles.title}>OOIOW A LA A</h1>
-      <span className={styles.price}>$15990</span>
+      <h1 className={styles.title}>{product.title}</h1>
+      <span className={styles.price}>${product.prices}</span>
     </div>
   )
 }
--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,23 +1,14 @@
 import styles from "../styles/ProductList.module.css"
 import ProductCard from "./ProductCard"
 
-const ProductList = () => {
+const ProductList = ({ productList }) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>BarAPP, tomemos... Buenas decisiones</h1>
       <div className={styles.wrapper}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {productList.map((product)=>(
+          <ProductList key={product._id} product={product} />
+        ))}
       </div>
     </div>
   )
