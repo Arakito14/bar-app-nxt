723d84cfc329c91083fff1a83781fed6202557ff
Arakito14 <inefableataraxia1@gmail.com>
2022-12-14 14:35:09 -0300

    added mesa modelo

--- models/Product.js
+++ models/Product.js
@@ -15,19 +15,6 @@ const ProductSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
-    statusm: {
-      type: [Boolean],
-      required: true,
-    },
-    mesas: {
-      type:[
-        {
-          text: {type: String, required: true},
-          statusm: {type: Boolean, required: true},
-        }
-      ]
-    }
-  },
   { timestamps: true }
 );
 
