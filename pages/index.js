e43ec84b5b03d0847c8b8bbaa99eeb0299c486fb
Arakito14 <inefableataraxia1@gmail.com>
2022-12-14 16:24:42 -0300

    clean

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
+import ProductList from '../components/ProductList'
 import Head from 'next/head'
-import Image from 'next/image'
 import Featured from '../components/Featured'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({productList}) {
   return (
     < div className = {styles.container} >      
       <Head>
@@ -12,6 +13,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured/>
+      <ProductList productList={productList}/>
     </div >
   )
 }
+
+export const getServerSideProps = async () => {
+  // const myCookie = ctx.req?.cookies || "";
+  // let admin = false;
+
+  // if (myCookie.token === process.env.TOKEN) {
+  //   admin = true;
+  // }
+
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      productList: res.data,
+      // admin,
+    },
+  };
+};
