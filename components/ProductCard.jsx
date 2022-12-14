c640a04d89501df4abec69e4e2776d26fd9ea7f3
Arakito14 <inefableataraxia1@gmail.com>
2022-12-14 16:46:31 -0300

    back-end click

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,9 +1,12 @@
 import styles from "../styles/ProductCard.module.css"
+import Link from "next/link"
 
 const ProductCard = ({ product }) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{product.title}</h1>
+      <Link href={`/product/${product._id}`} passHref>
+        <h1 className={styles.title}>{product.title}</h1>
+      </Link>
       <span className={styles.price}>${product.prices}</span>
     </div>
   )
